ec54adf43cef4bd7f393d8da72507889914d189e
Tais1990 <tais_kat@list.ru>
2019-04-18 16:29:25 +0500

    simple animation

---
+++ NetworkNew/Models/Particle.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkNew.Models
+{
+    public class Particle : BaseVM
+    {        
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Particle(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public Particle(Particle particle)
+        {
+            this.X = particle.X;
+            this.Y = particle.Y;
+        }
+
+        public Particle Move(double x, double y)
+        {
+            this.X = (this.X + x + 300) % 300;
+            this.Y = (this.Y + y + 300) % 300;
+            return this;
+        }
+    }
+    class Configuration : BaseVM
+    {
+        public Collection<Particle> particles { get; set; }
+        public int currentTime { get; set; }
+
+        public Configuration()
+        {
+            this.particles = new Collection<Particle>();
+            this.particles.Add(new Particle(50, 50));
+            this.particles.Add(new Particle(100, 100));
+            this.currentTime = 0;
+        }
+
+        public Configuration Move()
+        {
+            Random rnd = new Random();
+            int maxRendom = 100;
+            for (int i = 0; i < this.particles.Count; i++)
+            {
+                this.particles[i].Move(rnd.Next((-1) * maxRendom, maxRendom), rnd.Next((-1) * maxRendom, maxRendom));
+            }
+            this.currentTime++;
+            return this;
+        }
+        public Configuration(Configuration configuration)
+        {
+            this.particles = new Collection<Particle>();
+            for (int i = 0; i < configuration.particles.Count; i++)
+            {
+                this.particles.Add(new Particle(configuration.particles[i]));
+            }            
+            this.currentTime = configuration.currentTime;
+        }
+    }
+
+    class History : BaseVM
+    {
+        private int EndTime = 100;
+        public Collection<Configuration> history = new Collection<Configuration>();
+        public History()
+        {
+            history.Add(new Configuration());            
+            Configuration tmp;
+            for (int i = 0; i < EndTime; i++)
+            {                
+                tmp = this.history.LastOrDefault();
+                Configuration con = new Configuration(tmp);
+                history.Add(con.Move());
+            }
+        }
+    }
+}
---
+++ NetworkNew/ViewModels/MainViewModel.cs
@@ -0,0 +1,118 @@
+﻿using NetworkNew.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkNew.ViewModels
+{
+    public enum State { NotBegin, Play, Pause}
+    
+    class MainViewModel : BaseVM
+    {   
+        private State _state;        
+        public State State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                RaisePropertyChanged("State");
+            }
+        }        
+
+        public Configuration currentConfiguration;
+
+        public MainViewModel()
+        {
+            History history = new History();
+            currentConfiguration = new Configuration();
+            RaisePropertyChanged("currentConfiguration");
+            this.OnMove = false;
+            this.State = State.NotBegin;
+            foreach (Particle particle in this.Particles)
+            {
+                this._ParticleGraphics.Add(new ParticleGraphic(particle));
+            }
+            /*
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    Task.Delay(1000).Wait();                    
+                }
+            });
+            */
+        }
+
+        public bool OnMove { get; set; }
+        public DelegateCommand ToPlay
+        {
+            get
+            {
+                return new DelegateCommand((obj) =>
+                {
+                    this.OnMove = true;                    
+                    RaisePropertyChanged("OnMove");
+                    foreach (ParticleGraphic particle in this._ParticleGraphics)
+                    {
+                        particle.Change = true;
+                        particle.RaisePropertyChanged("Change");
+                    }
+                },
+                (obj) => !this.OnMove);
+            }
+        }
+        public DelegateCommand ToPause
+        {
+            get
+            {
+                return new DelegateCommand((obj) =>
+                {
+                    this.OnMove = false;
+                    
+                    RaisePropertyChanged("OnMove");
+                    foreach (ParticleGraphic particle in this._ParticleGraphics)
+                    {
+                        particle.Change = false;
+                        particle.RaisePropertyChanged("Change");
+                    }
+                },
+                (obj) => this.OnMove);
+            }
+        }
+
+        private Collection<ParticleGraphic> _ParticleGraphics = new Collection<ParticleGraphic>();
+
+
+
+        public ObservableCollection<Particle> Particles => new ObservableCollection<Particle>(currentConfiguration.particles);
+        public ObservableCollection<ParticleGraphic> ParticleGraphics => new ObservableCollection<ParticleGraphic>(_ParticleGraphics);
+
+        
+
+    }
+    public class ParticleGraphic : BaseVM
+    {
+        public string StartPoint { get; set; }
+        public string Points { get; set; }
+        public bool Change { get; set; }
+
+        public ParticleGraphic(Particle particle)
+        {
+            this.StartPoint = string.Format(@"{0}, {1}", particle.X, particle.Y);
+            this.Points = string.Format(@"{0},{1}", particle.X + 100, particle.Y + 100);
+            this.Change = false;
+        }
+
+        public void move()
+        {
+            this.Change = true;
+        }
+    }
+}
